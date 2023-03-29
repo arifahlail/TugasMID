41188e9381413fe18091187885cd5484133eefaa
arifahlail <127312731+arifahlail@users.noreply.github.com>
2023-03-28 23:51:56 -0700

    Update No2.java

--- No2.java
+++ No2.java
@@ -4,79 +4,70 @@
 
 public class Test0017 {
     public static void main(String[] args) {
-        // buat instansi objek balok1 dari Class Balok() →default const.
+        //a.instansi objek balok1
         Balok balok1 = new Balok();
         
-        // buat instansi objek balok2 dari Class Balok(20,35, 15)→param const.
+        //b.instansi objek balok2
         Balok balok2 = new Balok(20, 35, 15);
         
-        // set nilai l = 5 dari konstruktor Balok()
+        //c.set nilai l=5 
         balok1.setL(5);
         
-        // set nilai p = 25 dari konstruktor Balok()
+        //d.set nilai p=25 
         balok1.setP(25);
         
-        // set nilai t = 10 dari konstruktor Balok(20,35, 15)
+        //e.set nilai t=10 
         balok2.setT(10);
         
-        // panggil method info dari konstruktor Balok ()
+        //f.Memanggil method info dari konstruktor Balok()
         balok1.info();
         
-        // panggil method info dari konstruktor Balok(20,35, 15)
+        //g.Memanggil method info dari konstruktor Balok(20,35, 15)
         balok2.info();
     }
 }
-
 // class Balok
 class Balok extends PersegiPanjang {
     int t;
-    
     public Balok() {
-        // berisi nilai panjang & lebar yang diperoleh dari konstruktor superclass PersegiPanjang serta tinggi=15
+        //h. nilai panjang & lebar dari konstruktor superclass PersegiPanjang dan tinggi = 15
         super();
         this.t = 15;
-    }
-    
+    } 
     public Balok(int p, int l, int t) {
-        // berisi nilai pada saat runtime p <- p, l <- l dari konstruktor superclass PersegiPanjang t <- t;
+        //i.nilai pada saat runtime p <- p, l <- l dari konstruktor superclass PersegiPanjang t <- t;
         super(p, l);
         this.t = t;
     }
-    
-    // buat getter setter class balok
-    public int getT() {
+    //j.Membuat getter setter class balok
+    public int getC() {
         return t;
-    }
-    
-    public void setT(int t) {
+    }  
+    public void setC(int t) {
         this.t = t;
     }
-    
     public int volume() {
-        // fungsi untuk menghitung volume balok : p x l x t
-        return getP() * getL() * t;
-    }
-    
+        //k.fungsi menghitung volume balok : p x l x t
+  	     return getA() * getB() * t;
+    }    
     public void info() {
-        // fungsi untuk tampil nilai panjang, lebar, tinggi, Luas, dan volume balok
-        System.out.println("Nilai panjang: " + getP());
-        System.out.println("Nilai lebar: " + getL());
+        //l. fungsi untuk menampilkan nilai panjang, lebar, tinggi, Luas, dan volume balok
+        System.out.println("Nilai panjang: " + getA());
+        System.out.println("Nilai lebar: " + getB());
         System.out.println("Nilai tinggi: " + t);
         System.out.println("Nilai luas: " + luas());
         System.out.println("Nilai volume: " + volume());
     }
 }
-
 // class PersegiPanjang
 class PersegiPanjang {
-    int p, l;
-    
+    int p, l;  
     public PersegiPanjang() {
         // berisi nilai panjang=30 & lebar=20
         this.p = 30;
         this.l = 20;
     }
-    
+
     public PersegiPanjang(int p, int l) {
         // berisi nilai pada saat runtime p <- p, l <- l
         this.p = p;
@@ -84,22 +75,19 @@ class PersegiPanjang {
     }
     
     // buat getter setter class PersegiPanjang
-    public int getP() {
+    public int getA() {
         return p;
     }
     
-    public void setP(int p) {
+    public void setA(int p) {
         this.p = p;
     }
-    
-    public int getL() {
+    public int getB() {
         return l;
     }
-    
-    public void setL(int l) {
+    public void setB(int l) {
         this.l = l;
     }
-    
     public int luas() {
         // fungsi untuk menghitung luas persegi panjang : p x l
         return
